dd57c86572c9d3b91039336ce416de32c8e03006
mohits15 <56106307+mohits15@users.noreply.github.com>
2019-10-07 12:20:13 +0530

    Add files via upload

--- Assignment1.c
+++ Assignment1.c
@@ -1,61 +1,60 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#define SIZE 99999
-void main(){
-    char s[SIZE];
-    int a[SIZE],b[SIZE],c[SIZE];
-    char* part1,*part2,*part3,*part4;
-    int i1,i2,i=2,j,k,c1=0;
-    int p=0,n;
-    printf("\nEnter any integer value of K:\n");
-    scanf("%d",&k);
-    for(i=2;k>1;i++){
-        while(k%i==0){
-            b[i-2]=i;
-            k=k/i;
-            printf("%d ",b[i-2]);
-        }
-    }
-    printf("\nEnter the queries values like: ' ! l r x ' or  ' ? l r ':\n");
-    fflush(stdin); //Buffer clear
-    printf("\nEnter the queries:= ");
-    gets(s);
-    part1=strtok(s," ");
-    // IF the input string is ! l r x
-    if(*part1=='!'){
-        part2=strtok(NULL," ");
-        part3=strtok(NULL," ");
-        part4=strtok(NULL," ");
-        int p1=*part2;
-        int p2=*part3;
-        int p3=*part4;
-        p1=p1-48,p2=p2-48,p3=p3-48;
-        printf("\nThe value is assigned to position\n");
-        for(p=p1;p<=p2;p++){
-            c[p]=p3;
-            printf("array[%d]=%d\n",p,c[p]);
-        }
-        printf("\nThe string look likes=");
-        for(n=0;n<=p2;n++){
-            printf("%d ",c[n]);
-        }
-    }
-    //IF the input string is ? l r
-    if(*part1=='?'){
-        part2=strtok(NULL," ");
-        part3=strtok(NULL," ");
-        int l1=*part2;
-        int r=*part3;
-        l1=l1-48;r=r-48;
-        for(i1=0;i1<=r;i1++){
-            for(i2=0;i2<=r;i2++){
-                if(b[i2]==l1){
-                    c1++;
-                }
+#include<math.h>
+void prime(int);
+void main()
+{
+    int k,q,i=0;
+    char s[50];
+    int a[10],*t;
+    int A[1000];
+    printf("\n Enter the values for K and Q");
+    scanf("%d%d",&k,&q);
+    printf("\nEnter the queries in the form of \n ! l r x OR ? l r");
+    t=prime(k);
+    while(i<q)
+    {
+        printf("Enter the %d query",i);
+        i++;
+        scanf("%s",s);
+        sscanf(s,"%d %d %d %d",&a[0],&a[1],&a[2],&a[3]);
+        if(a[0]=='!')
+        {
+            for(int j=a[1];j<=a[2]+1;j++)
+            {
+                A[j]=a[3];
+                printf("%d",a[j])
             }
-            l1++;
         }
-        printf("\nThe number of common prime factors= %d ",c1);
+        int c=0;
+        if(a[0]=='?')
+        {
+           int s=0;
+           for(i=0;i<=a[2];i++)
+           {
+               if(b[i]==a[1])
+               {
+                   c++;
+               }
+           }
+           printf("no of common prime factors is %d",c);
+        }
+
+
     }
+}
+void prime(int n)
+{
+    int b[20],i=0;
+    for(i=2;n>1;i++)
+    {
+        while(n%i==0)
+        {
+            b[i-2]=i;
+            n=n/i;
+        }
+    }
+    return b;
+
 }
