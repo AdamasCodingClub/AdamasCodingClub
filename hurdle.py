0e2e115f49845b1879504c30edf6e3eb648fce71
Mili Dasmahapatra <56042800+mili1999@users.noreply.github.com>
2019-10-07 20:31:54 +0530

    Add files via upload

--- hurdle.py
+++ hurdle.py
@@ -1,36 +1,36 @@
-import math
+l=int(input("Enter the array size ::"))
+n=int(input("enter the number"))
+k=[]
+for i in range(2,n+1):
+    if(n>1):
+        while(n%i==0):
+            k.append(i)
+            n=n//i
+print(k)
 
-a=[]
-r=int(input("Enter the no of elements for the array:"))
+q=str(input("Enter the string :"))
+k1=[]
+q1=int(q[2])
+if(q[0]=='!'):
+    x=int(q[6])
+    r=int(q[4])
+    for j in range(0,r):
+        if(q1==j):
+           k1.append(x)
+           q1=q1+1
+        else:
+            k1.append(" ")
+    print(k1)
+elif(q[0]=='?'):
+    start=int(q[2])
+    end=int(q[4])
+    c=0
+    for i in range(start,end+1):
+        for j in range(0,len(k)):
+            match=int(k[j])
+            if(match==i):
+                if(int(k[j])!=int(k[j-1])):
+                    c=c+1
+    print(c)
+                    
 
-for i in range(r):
-	n=int(input("Enter the element:"))
-	a.append(n)
-print(a)
-
-k=int(input("Enter the positive integer number"))
-while k % 2==0:
-	print(2)
-	k=k/2
-for j in range(3,int(math.sqrt(k))+1,2):
-	while k % j==0:
-		print("Prime Factors:")
-		for l in a:
-			if j==l:
-				print(j)
-		k=k/j
-if k>2:
-	for l in a:
-		if k==l:
-			print(int(k))
-
-
-
-
-
-
-
-
-			
-		
-	
