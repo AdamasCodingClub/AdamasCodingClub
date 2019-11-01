5cf221251314c28833ed16c9f3ba189c325e716b
Sanmoy <55270598+Sanmoy1@users.noreply.github.com>
2019-11-01 22:58:30 +0530

    Create hurdle2.py

---
+++ hurdle2.py
@@ -0,0 +1,19 @@
+import nltk
+def filecreate():
+  with open('file.txt','r')
+  file.write(input())
+  file.close()
+filecreate()
+x=file.read
+r1=nltk.word_tokenize(read)
+length=len(r1)
+
+p1=nltk.pos_tag(r1)
+ar1[]
+ar2[]
+if(p1=='NN' || p1=='NNP'):
+  ar1.append(r)
+for i in ar1:
+    if i not in ar2:
+        ar2.append(i)   
+print(ar2,sep='\n')        
